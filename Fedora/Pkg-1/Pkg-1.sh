89703f8a4cb69fd58ce13e7333710e56e72e2ca6
JahsDalmation <kalani.hokianga@gmail.com>
2022-02-02 04:46:39 +1300

    Test Edit

--- Fedora/Pkg-1/Pkg-1.sh
+++ Fedora/Pkg-1/Pkg-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # A bash script to set up Fedora ( Spotify, Steam, Tweaks & Extenstions w/ Nvidia drivers )
-
+# 
 # Make sure all scripts are executable ( Can be done in most file explorers in properties -> permissions )
 # This script will not run otherwise
 

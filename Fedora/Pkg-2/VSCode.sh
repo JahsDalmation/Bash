b5e925c60d4e610544c8b84ccfa2dcae5608dda2
JahsDalmation <kalani.hokianga@gmail.com>
2022-02-02 17:25:58 +1300

    VSCode - script completed

--- Fedora/Pkg-2/VSCode.sh
+++ Fedora/Pkg-2/VSCode.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 # A Simple bash script to install Microsoft's Visual Studio Code
+
+# Install the key for VSCode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Install the VSCode repository
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# Update DNF cache and check for updates
+dnf check-update
+
+# Install VSCode
+sudo dnf install code -y

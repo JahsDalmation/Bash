67a95f12fbf3cb4581a8c46fdeab6da35215c755
JahsDalmation <kalani.hokianga@gmail.com>
2022-02-02 17:45:54 +1300

    Fedora.sh - script complete

---
+++ Fedora/Fedora.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# The goal of this script is to install all the packages from Pkg-1 & Pkg-2
+# Make sure all scripts are executable ( Can be done in most file explorers in properties -> permissions )
+# This script will not run otherwise
+
+# Run Pkg-1.sh
+./Pkg-1/Pkg-1.sh
+
+# Run Pkg-2.sh
+./Pkg-2/Pkg-2.sh

efeecb4659cfff467a21f049f7b0269d5715616a
JahsDalmation <kalani.hokianga@gmail.com>
2022-02-02 17:52:26 +1300

    Fedora.sh - script to run from Fedora directory

--- Fedora/Fedora.sh
+++ Fedora/Fedora.sh
@@ -5,7 +5,18 @@
 # This script will not run otherwise
 
 # Run Pkg-1.sh
-./Pkg-1/Pkg-1.sh
+
+cd ./Pkg-1
+./Pkg-1.sh
+cd ..
 
 # Run Pkg-2.sh
-./Pkg-2/Pkg-2.sh
+cd ./Pkg-2
+./Pkg-2.sh
+cd ..
+
+# Clear screen
+clear 
+
+# Display complete
+echo 'complete'
